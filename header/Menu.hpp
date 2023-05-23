f43d93cebc2d7d4d7e5c66987b0322243bdf041e
Lietrix <joshdot@gmail.com>
2023-05-22 17:14:06 -0700

    Made Menu and Piece files, some code for Menu

---
+++ header/Menu.hpp
@@ -0,0 +1,11 @@
+#ifndef MENU_HPP
+#define MENU_HPP
+
+namespace Menu {
+    void displayMenu();
+    void singlePlayer();
+    void twoPlayer();
+    void quit();
+}
+
+#endif
---
+++ src/Menu.cpp
@@ -0,0 +1,24 @@
+#include "Menu.hpp"
+#include <iostream>
+using namespace std;
+
+void Menu::displayMenu() {
+    cout << "=== Game Menu ===" << endl;
+    cout << "1. Single Player" << endl;
+    cout << "2. TwoPlayer" << endl;
+    cout << "3. Quit" << endl;
+    cout << "=================" << endl;
+    cout << "Enter your choice: ";
+}
+
+void Menu::singlePlayer(){
+
+}
+
+void Menu::twoPlayer() {
+
+}
+
+void Menu::quit() {
+
+}
