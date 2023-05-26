f0bb4aa6ed6e37d45ac645e444a536f00286b2d5
david1u <129913826+david1u@users.noreply.github.com>
2023-05-26 00:42:59 -0700

    Update Piece.hpp

--- header/Piece.hpp
+++ header/Piece.hpp
@@ -1,6 +1,20 @@
 #ifndef __PIECE_HPP__
 #define __PIECE_HPP__
 
+#define BLACK_KING   "\u2654"   // ♔
+#define BLACK_QUEEN  "\u2655"   // ♕
+#define BLACK_ROOK   "\u2656"   // ♖
+#define BLACK_BISHOP "\u2657"   // ♗
+#define BLACK_KNIGHT "\u2658"   // ♘
+#define BLACK_PAWN   "\u2659"   // ♙
+
+#define WHITE_KING   "\u265A"   // ♚
+#define WHITE_QUEEN  "\u265B"   // ♛
+#define WHITE_ROOK   "\u265C"   // ♜
+#define WHITE_BISHOP "\u265D"   // ♝
+#define WHITE_KNIGHT "\u265E"   // ♞
+#define WHITE_PAWN   "\u265F"   // ♟
+
 #include <string>
 #include "Board.hpp"
 
@@ -29,7 +43,7 @@ class Pawn : public Piece
 public:
    Pawn(bool isWhite)
    {
-      name = isWhite ? '♟︎' : '♙';
+      name = isWhite ? WHITE_PAWN : BLACK_PAWN;
       white = isWhite;
    }
    bool move(int newX, int newY, Board &board) override;
@@ -40,7 +54,7 @@ class Rook : public Piece
 public:
    Rook(bool isWhite)
    {
-      name = isWhite ? '♜' : '♖';
+      name = isWhite ? WHITE_ROOK : BLACK_ROOK;
       white = isWhite;
    }
    bool move(int newX, int newY, Board &board) override;
@@ -51,7 +65,7 @@ class Knight : public Piece
 public:
    Knight(bool isWhite)
    {
-      name = isWhite ? '♞' : '♘';
+      name = isWhite ? WHITE_KNIGHT : BLACK_KNIGHT;
       white = isWhite;
    }
    bool move(int newX, int newY, Board &board) override;
@@ -62,7 +76,7 @@ class Bishop : public Piece
 public:
    Bishop(bool isWhite)
    {
-      name = isWhite ? '♝' : '♗';
+      name = isWhite ? WHITE_BISHOP : BLACK_BISHOP;
       white = isWhite;
    }
    bool move(int newX, int newY, Board &board) override;
@@ -73,7 +87,7 @@ class Queen : public Piece
 public:
    Queen(bool isWhite)
    {
-      name = isWhite ? '♛' : '♕';
+      name = isWhite ? WHITE_QUEEN : BLACK_QUEEN;
       white = isWhite;
    }
    bool move(int newX, int newY, Board &board) override;
@@ -84,7 +98,7 @@ class King : public Piece
 public:
    King(bool isWhite)
    {
-      name = isWhite ? '♚' : '♔';
+      name = isWhite ? WHITE_KING : BLACK_KING;
       white = isWhite;
    }
    bool move(int newX, int newY, Board &board) override;
