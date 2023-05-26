c95a10af444cf65b64c74d55d2e2a6b9a6c80c62
matthewc2003 <matthewc2003@gmail.com>
2023-05-26 12:13:27 -0700

    Implemented move function and showElminated function

--- header/Player.hpp
+++ header/Player.hpp
@@ -2,6 +2,7 @@
 #define PLAYER_HPP
 #include <iostream>
 #include <vector>
+#include <map>
 #include "Piece.hpp"
 
 class Player {
@@ -9,11 +10,16 @@ class Player {
     string name;
     //true represents white piece, false represents black piece
     bool blackWhite;
-    vector<Piece*> eliminated;
+    vector<Piece*> eliminaated;
+    map<char, int> letterToNum = {
+        {'a', 0}, {'b', 1}, {'c', 2}, {'d', 3},
+        {'e', 4}, {'f', 5}, {'g', 6}, {'h', 7}
+    };
  public:
     void makeMove();
     void surrender();
     void addEliminated(Piece*);
+    void showEliminated();
 
     ~Player();
 };
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,8 +1,34 @@
 #include "Player.hpp"
+#include <map>
 
 using namespace std;
 
 void Player::makeMove() {
+    char letter;
+    int number;
+    pair<int, int> start, dest;
+    cout << "Enter starting position (letter number): ";
+    cin >> letter >> number;
+    start = make_pair(letterToNum[letter], number - 1);
+
+    if (start.first < 0 || start.first > 7 || start.second < 0 || start.second > 7) {
+            cout << "Invalid starting position.\n";
+            return;
+    }
+
+    Piece* piece = board->getPiece(start.first, start.second); 
+
+    cout << "Enter destination position (letter number): ";
+    cin >> letter >> number;
+    dest = make_pair(letterToNum[letter], number - 1);
+
+    if (dest.first < 0 || dest.first > 7 || dest.second < 0 || dest.second > 7) {
+            cout << "Invalid destination position.\n";
+            return;
+    }
+
+    piece->move(dest.first, dest.second);
+
 
 }
 
@@ -12,6 +38,19 @@ void Player::surrender() {
 
 void Player::addEliminated(Piece* piece) {
     eliminated.push_back(piece);
+}
+
+void Player::showEliminated() const{
+    if (eliminated.empty()) {
+            cout << "No pieces have been eliminated.\n";
+            return;
+        }
+        
+    cout << "Eliminated pieces: ";
+    for (const auto& piece : eliminated) {
+        cout << piece->getName() << " ";
+    }
+    cout << "\n";
 }
 
 Player::~Player() {
