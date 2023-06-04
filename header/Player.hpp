a15d1b124a24c954a3d4c86db46d802a1a79da19
matthewc2003 <matthewc2003@gmail.com>
2023-06-04 15:11:55 -0700

    Added more validation to the Playermove function

--- header/Player.hpp
+++ header/Player.hpp
@@ -4,19 +4,21 @@
 #include <vector>
 #include <map>
 #include "Piece.hpp"
+#include "Board.hpp"
 
 class Player {
  private:
     string name;
     //true represents white piece, false represents black piece
     bool blackWhite;
-    vector<Piece*> eliminaated;
+    vector<String> eliminated;
     map<char, int> letterToNum = {
         {'a', 0}, {'b', 1}, {'c', 2}, {'d', 3},
         {'e', 4}, {'f', 5}, {'g', 6}, {'h', 7}
     };
  public:
-    void makeMove();
+    bool getColor();
+    void makeMove(Board*);
     void surrender();
     void addEliminated(Piece*);
     void showEliminated();
--- src/Player.cpp
+++ src/Player.cpp
@@ -3,7 +3,11 @@
 
 using namespace std;
 
-void Player::makeMove() {
+bool Player::getColor() {
+    return blackWhite;
+}
+
+bool Player::makeMove(Board* board) {
     char letter;
     int number;
     pair<int, int> start, dest;
@@ -11,10 +15,18 @@ void Player::makeMove() {
     cin >> letter >> number;
     start = make_pair(letterToNum[letter], number - 1);
 
+    // Check if the soruce coordinates are on the board
     if (start.first < 0 || start.first > 7 || start.second < 0 || start.second > 7) {
             cout << "Invalid starting position.\n";
-            return;
+            return false;
     }
+
+    // Check if you are moving your own piece
+    if(board->getPiece(start.fist, start.second)->getColor() != this->getColor()) {
+            cout << "Not a valid piece to move or no piece exists\n";
+            return false;
+    }
+
 
     Piece* piece = board->getPiece(start.first, start.second); 
 
@@ -22,22 +34,38 @@ void Player::makeMove() {
     cin >> letter >> number;
     dest = make_pair(letterToNum[letter], number - 1);
 
+    // Check if the destination is on the board
     if (dest.first < 0 || dest.first > 7 || dest.second < 0 || dest.second > 7) {
             cout << "Invalid destination position.\n";
-            return;
+            return false;
     }
 
-    piece->move(dest.first, dest.second);
+    // Check if it's a valid move 
+    if(!(piece->move(dest.first, dest.second))) {
+        return false;
+    }
+
+    // Check for collision
+    if(!board->isFree(dest.first, dest.second)){
+        Piece* toBeRemoved = board->getPiece(dest.first, dest.second);
+        addEliminated(toBeRemoved);
+    }
+
+    updateBoard(dest.first, dest.second, piece);
+    
+    // move successful
+    return true;
 
 
 }
 
 void Player::surrender() {
-
+    
 }
 
 void Player::addEliminated(Piece* piece) {
-    eliminated.push_back(piece);
+    eliminated.push_back(piece->getName());
+    delete piece;
 }
 
 void Player::showEliminated() const{
@@ -47,7 +75,7 @@ void Player::showEliminated() const{
         }
         
     cout << "Eliminated pieces: ";
-    for (const auto& piece : eliminated) {
+    for (int x; x < eliminated.len(); x++) {
         cout << piece->getName() << " ";
     }
     cout << "\n";
