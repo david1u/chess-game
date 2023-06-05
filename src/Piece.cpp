4da54f9c212f5cef46c219f7fc2498f7e0041933
akothule <ayush.kothule@gmail.com>
2023-06-04 18:58:50 -0700

    Fixed #42

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -16,24 +16,24 @@ bool Pawn::move(int newX, int newY, Board &board) {
         jump2 = true;
     }
     if(this->getColor()){
-        if(this->getXCoord() - newX == 0 && this->getYCoord() - newY == -2){
-        if(jump2){
-            return true;
-        }
-        return false;
-        }
-        if(this->getXCoord() - newX == 0 && this->getYCoord() - newYg == -1){
-            return true;
-        }
-    }
-    else{
         if(this->getXCoord() - newX == 0 && this->getYCoord() - newY == 2){
         if(jump2){
             return true;
         }
         return false;
         }
-        if(this->getXCoord() - newX == 0 && this->getYCoord() - newYg == 1){
+        if(this->getXCoord() - newX == 0 && this->getYCoord() - newY == 1){
+            return true;
+        }
+    }
+    else{
+        if(this->getXCoord() - newX == 0 && this->getYCoord() - newY == -2){
+        if(jump2){
+            return true;
+        }
+        return false;
+        }
+        if(this->getXCoord() - newX == 0 && this->getYCoord() - newY == -1){
             return true;
         }
     }
